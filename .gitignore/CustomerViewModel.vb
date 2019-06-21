2987c64a1880c7bb4620089c65dc7cadeba7bc08
rw1202 <44084938+rw1202@users.noreply.github.com>
2019-06-21 15:32:11 +0100

    Add files via upload
    Complete

---
+++ .gitignore/CustomerViewModel.vb
@@ -0,0 +1,18 @@
+﻿Imports AdventureWorks.EntityLayer
+Imports Common.Library
+Public Class CustomerViewModel
+    Inherits ViewModelBase
+    Sub New ()
+        LoadCustomer(1)
+        End Sub
+    Public Property Entity As Customer 
+    
+    Function LoadCustomer(ByVal customerId As Integer) As Customer
+
+        Entity = New Customer() With {.CustomerID= 1,
+            .FirstName="James", .LastName= "Bond", .CompanyName="DW"
+            }
+        Return Entity
+                End Function
+
+End Class
---
+++ .gitignore/ProductViewModel.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Collections.ObjectModel
+Imports AdventureWorks.DataLayer
+Imports AdventureWorks.EntityLayer
+Imports Common.Library
+Public Class ProductViewModel
+    Inherits ViewModelBase
+
+    Sub New
+        LoadProducts()
+    End Sub
+
+    Public Property Products As ObservableCollection(Of Product)
+    Public Property Entity As Product
+
+    Function LoadProducts() As ObservableCollection(Of Product)
+        Return LoadProducts()
+    End Function
+
+    Function LoadProducts(ByVal startingFilePath As String) _
+        As ObservableCollection(Of Product)
+        Dim mgr = New ProductManager
+        Products = mgr.LoadProducts(startingFilePath)
+        Return Products
+    End Function
+    '    Function LoadProduct(ByVal ProductId As Integer) As Product
+    '        Return LoadProduct(ProductId)
+    '
+    '    End Function
+
+    '    Function LoadProduct(ByVal ProductId As Integer, _
+    '                         ByVal startingFilePath As String) As Product
+    '        Entity = New Product() With {.ProductId = 680,
+    '            .ListPrice = 100, .Name = "10 Speed Bike",
+    '            .IsActive = True, .ProductNumber = "500"
+    '                        }
+    '        Return Entity
+    '    End Function
+End Class
